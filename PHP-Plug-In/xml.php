5920884ad7d08a9aeb27c904d22d1715e4cdd988
gregor <k3mpaxl@googlemail.com>
2012-12-11 13:30:23 +0100

    / ‘PHP-Plug-In/xml.php’

--- PHP-Plug-In/xml.php
+++ PHP-Plug-In/xml.php
@@ -20,6 +20,7 @@ $login=$ilUser->getLogin();
 $xml = new SimpleXMLElement("<Iliconnect/>");
 # CURRENT
 $current = $xml->addChild("Current");
+$notifications = $current->addChild("Notifications");
 $desktop = $current->addChild("Desktop");
 
 /*
@@ -63,26 +64,30 @@ foreach($newsitems as $newsitem) {
 
 
 
+
 #desktopItem2Xml($ilUser->getDesktopItems(),$desktop);
 
+## PERSOENLICHER SCHREIBTISCH AUSGEBEN
 $desktop_items= $ilUser->getDesktopItems();
 global $tree;
 
-$desktop_items= $tree->getChilds(1);
+
+## KOMPLETTES MAGAZIN AUSGEBEN
+#$desktop_items= $tree->getChilds(1);
 
 foreach($desktop_items as $item) {
-  desktopItem2Xml($item,$desktop);
+  desktopItem2Xml($item,$desktop,$notifications);
 }
 
-
-echo $xml->asXML();
+## GIBT DIE XML AUS
+#echo $xml->asXML();
 
 
 #print_r($children);
 
 
 #global $ilDB;
-#$q = "SELECT obj_id,ref_id FROM object_reference WHERE ".$ilDB->in("ref_id", array(49), false, 'integer');
+#$q = "SELECT obj_id,ref_id FROM object_reference WHERE ".$ilDB->in("ref_id", array(63), false, 'integer');
 #$r = $ilDB->query($q);
 #$elem=$ilDB->fetchAssoc($r);
 
@@ -90,17 +95,52 @@ echo $xml->asXML();
 #print_r($elem);
 
 require_once('classes/class.ilObjectFactory.php');
-$factory=new ilObjectFactory();
-$object=$factory->getInstanceByRefId(63);
+require_once('Modules/Exercise/classes/class.ilObjExerciseAccess.php');
+require_once('Modules/Exercise/classes/class.ilObjExerciseGUI.php');
+require_once('Modules/Exercise/classes/class.ilExAssignment.php');
+
+#$factory=new ilObjectFactory();
+#$object=$factory->getInstanceByRefId(63);
+
+
+#$assarray = new ilExAssignment($object);
+#print_r($ass);
+
+#$test = new ilExAssignment(63);
+
+#print_r($object->getOwner());
+
+#print_r($object);
+#$notify = $notifications->addChild("Notification");
+#$notify->addChild("title",$assarray->getTitle());
+#$notify->addChild("description",$assarray->getDescription());
+#$notify->addChild("date",$assarray->getDeadline());
+#$notify->addChild("ref_id",$assarray->getId());
+
+
+echo $xml->asXML();
+
+#global $lng, $ilUser;
+
+#$props = array();
+#$rem = ilObjExerciseAccess::_lookupRemainingWorkingTimeString($object->obj_id);
+#$props[] = array(
+#"property" => $object->lng->txt("exc_next_deadline"),
+#"value" => ilObjExerciseAccess::_lookupRemainingWorkingTimeString($object->obj_id)
+#);
+
+#print_r($rem);
 
 #print_r($object);
 #$filesize=$this->format_bytes($object->getFileSize());
 
-$ex_gui =& new ilObjExerciseGUI("", (int) $_GET["ref_id"], true, false);
+#$ex_gui =& new ilObjExerciseGUI("", (int) $_GET["ref_id"], true, false);
 
-print_r($ex_gui);
+#print_r($ex_gui->read());
 
-function desktopItem2Xml($array,$sxml){
+
+## FUNCTION FUER DIE REKURSIVE ABARBEITUNG DER CHILD ITEMS
+function desktopItem2Xml($array,$sxml,$notifications){
   if(strstr("file|fold|crs|tst|exc",$array["type"]))
   {
     $item = $sxml->addChild("Item");
@@ -108,11 +148,24 @@ function desktopItem2Xml($array,$sxml){
     $item->addChild("description",$array[description]);
     $item->addChild("type",$array[type]);
     $item->addChild("ref_id",$array[ref_id]);
+    if($array[type] == "exc")
+    {
+      $factory=new ilObjectFactory();
+      $exc=$factory->getInstanceByRefId($array[ref_id]);
+      require_once('Modules/Exercise/classes/class.ilExAssignment.php');
+      $assarray = new ilExAssignment($exc);
+
+      $notify = $notifications->addChild("Notification");
+      $notify->addChild("title",$assarray->getTitle());
+      $notify->addChild("date",$assarray->getDeadline());
+      $notify->addChild("ref_id",$assarray->getId());
+    }
+    
     global $tree;
     $children=$tree->getChilds($array[ref_id]);
     foreach($children as $child)
     {
-      desktopItem2Xml($child,$item);
+      desktopItem2Xml($child,$item,$notifications);
     }
   } else {
     # Debugging

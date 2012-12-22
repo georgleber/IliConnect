6eed1eee8c8e579f10b46c47decd2e255accb46c
gregor <k3mpaxl@googlemail.com>
2012-12-22 16:07:07 +0100

    / ‘PHP-Plug-In/class.IliConnect.php’

--- PHP-Plug-In/class.IliConnect.php
+++ PHP-Plug-In/class.IliConnect.php
@@ -2,6 +2,26 @@
 
 class IliConnect{
 	
+	function __construct(){
+
+		switch($_GET["action"]) {
+			case "join":
+				echo joinCourse($course, isset($_GET["course_pw"]) ? $_GET["course_pw"] : null);
+				break;
+			case "leave":
+				echo leaveCourse($course);
+				break;
+			case "sync":
+				break;
+			case "mgzn":
+				break;
+			default:
+				echo "ACTION_UNKNOWN";
+				break;
+		}
+
+	}
+	
 }
 
 ?>

66679b0a29a3c5ee324e1e7f5f000adb64adc322
Doxlike <Pierre.schlimmer@gmail.com>
2013-01-14 13:02:33 +0100

    IliOnClickListener.java

---
+++ IliConnect/src/com/android/iliConnect/message/IliOnClickListener.java
@@ -0,0 +1,8 @@
+package com.android.iliConnect.message;
+
+public interface IliOnClickListener {
+	
+	public void onClickCoursePassword(String refID,String password);
+	public void onClickJoinCourse(String refID, String courseName);
+	public void onClickLeftCourse(String refID, String courseName);
+}
